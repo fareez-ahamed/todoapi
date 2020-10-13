28fe925a689772f9e2ca2a95de0e2f2b7b10603b
Fareez Ahamed <fareez.ahamed@gmail.com>
2020-10-13 22:37:01 +0530

    removed api server

--- main.go
+++ main.go
@@ -6,29 +6,14 @@ import (
 	"github.com/fareez-ahamed/todoapi/store/sqlite"
 )
 
-// APIServer represents the Rest API Server
-type APIServer struct {
-	Port    int
-	Handler http.Handler
-}
-
 func main() {
 	store := sqlite.NewStore("test.db")
-	todoHandler := TodoHandler{
+	todoHandler := &TodoHandler{
 		Store: store,
 	}
-	api := APIServer{
-		Port:    8080,
-		Handler: &todoHandler,
-	}
-	api.Listen()
-}
-
-// Listen starts the api server
-func (s *APIServer) Listen() {
 	httpServer := http.Server{
 		Addr:    ":8080",
-		Handler: s.Handler,
+		Handler: todoHandler,
 	}
 	httpServer.ListenAndServe()
 }
